f9db411d4296ad94cfe283c8b5eba9254027106f
zethporter <zethieboy@gmail.com>
2020-07-27 21:07:40 -0600

    fixed some HTML, rebuilt docker container

--- ui/src/App.js
+++ ui/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Just a Test.
         </p>
         <a
           className="App-link"
@@ -20,6 +20,7 @@ function App() {
         </a>
       </header>
     </div>
+    
   );
 }
 
